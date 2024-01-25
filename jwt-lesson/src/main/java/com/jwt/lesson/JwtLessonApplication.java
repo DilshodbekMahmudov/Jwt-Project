d94c1fa5a1e34937a9d427f5cb2f3a3d330a1457
Dilshodbek Mahmudov <155676392+DilshodbekMahmudov@users.noreply.github.com>
2024-01-25 23:33:22 +0500

    Add files via upload

---
+++ jwt-lesson/src/main/java/com/jwt/lesson/JwtLessonApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtLessonApplication.class, args);
+	}
+
+}
---
+++ jwt-lesson/src/main/java/com/jwt/lesson/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jwt.lesson.repository;
+
+import com.jwt.lesson.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String username);
+}
